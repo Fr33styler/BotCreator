35cb6c041485b1b75a0732dc95c7ad8796f7bd58
Fr33styler <Fr33stylerMC@gmail.com>
2026-03-28 00:13:40 +0200

    fix: Removed get System look and feel to hopefully fix issues on windows

--- bot-creator-main/src/main/java/ro/fr33styler/botcreator/Main.java
+++ bot-creator-main/src/main/java/ro/fr33styler/botcreator/Main.java
@@ -26,10 +26,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ignored) {}
-
         JFrame frame = new JFrame("BotCreator");
 
         frame.setResizable(false);
